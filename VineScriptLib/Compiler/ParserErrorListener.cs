19d1e7aa146ba35323c2cca80119033d81c6a14f
Julien Samama <allinlabs@gmail.com>
2017-04-19 01:44:50 +0200

    Make ParserErrorListener public + renaming vars

--- VineScriptLib/Compiler/ParserErrorListener.cs
+++ VineScriptLib/Compiler/ParserErrorListener.cs
@@ -3,12 +3,12 @@ using Antlr4.Runtime;
 
 namespace VineScriptLib
 {
-    class ParserErrorListener : BaseErrorListener
+    public class ParserErrorListener : BaseErrorListener
     {
-        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string msg, RecognitionException e)
+        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string desc, RecognitionException e)
         {
-            string m = string.Format("Invalid Expression at {0}:{1}, {2}", line, charPositionInLine, msg);
-            throw new ArgumentException(m, msg, e);
+            string msg = string.Format("[Parser] Invalid Expression at {0}:{1}, {2}", line, charPositionInLine, desc);
+            throw new ArgumentException(msg, desc, e);
         }
     }
 }
