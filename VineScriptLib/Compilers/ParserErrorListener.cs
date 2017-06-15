4d5f115f3c7981be9422ba08f20b7ebb2d0b767a
Julien Samama <allinlabs@gmail.com>
2017-06-15 19:04:25 +0200

    New error listener for the parser that underline the error

--- VineScriptLib/Compilers/ParserErrorListener.cs
+++ VineScriptLib/Compilers/ParserErrorListener.cs
@@ -5,10 +5,14 @@ namespace VineScriptLib.Compilers
 {
     class ParserErrorListener : BaseErrorListener
     {
-        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, int line, int charPositionInLine, string desc, RecognitionException e)
+        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, 
+            int line, int charPositionInLine, string msg, RecognitionException e)
         {
-            string msg = string.Format("[Parser] Invalid Expression at {0}:{1}, {2}", line, charPositionInLine, desc);
-            throw new ArgumentException(msg, desc, e);
+            string errmsg = string.Format(
+                "[Parser] Invalid Expression at {0}:{1}, {2}", 
+                line, charPositionInLine, msg
+            );
+            throw new Exception(errmsg, e);
         }
     }
 }
--- VineScriptLib/Compilers/Vine/VineCompiler.cs
+++ VineScriptLib/Compilers/Vine/VineCompiler.cs
@@ -3,6 +3,7 @@ using Antlr4.Runtime;
 using System;
 using System.Collections.Generic;
 using VineScriptLib.Core;
+using VineScriptLib.Compilers;
 using VineScriptLib.Compilers.Vine;
 
 namespace VineScriptLib.Compilers.Vine
@@ -14,6 +15,7 @@ namespace VineScriptLib.Compilers.Vine
         private CommonTokenStream tokens;
         private VineParser parser;
         private ParserErrorListener errorListener;
+        private UnderlineErrorListener underlineListener;
 
         private void Setup(string text)
         {
@@ -21,10 +23,12 @@ namespace VineScriptLib.Compilers.Vine
             lexer = new VineLexer(inputStream);
             tokens = new CommonTokenStream(lexer);
             parser = new VineParser(tokens);
-
-            errorListener = new ParserErrorListener();
-            parser.RemoveErrorListeners();
-            parser.AddErrorListener(errorListener);
+            
+            //errorListener = new ParserErrorListener();
+            underlineListener = new UnderlineErrorListener();
+            parser.RemoveErrorListeners(); // remove ConsoleErrorListener
+            //parser.AddErrorListener(errorListener);
+            parser.AddErrorListener(underlineListener);
         }
 
         public string Parse(string text, VineStory story)
