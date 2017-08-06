d812b3f1e0468413572c71767ef3fdf03f456a25
Julien Samama <allinlabs@gmail.com>
2017-08-06 19:54:07 +0200

    Optimized parsing with a first run using the quick SLL, and if it fails, use LL

---
+++ VineScript/Compiler/ErrorListeners/BailErrorListener.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Antlr4.Runtime;
+
+namespace VineScript.Compiler
+{
+    internal class BailErrorListener : BaseErrorListener
+    {
+        public override void SyntaxError(IRecognizer recognizer, IToken offendingSymbol, 
+            int line, int column, string errmsg, RecognitionException e)
+        {
+            throw new Antlr4.Runtime.Misc.ParseCanceledException(errmsg);
+        }
+    }
+}
--- VineScript/Compiler/VineCompiler.cs
+++ VineScript/Compiler/VineCompiler.cs
@@ -3,6 +3,8 @@ using Antlr4.Runtime;
 using System;
 using System.Collections.Generic;
 using VineScript.Core;
+using Antlr4.Runtime.Atn;
+using Antlr4.Runtime.Misc;
 
 namespace VineScript.Compiler
 {
@@ -55,6 +57,48 @@ namespace VineScript.Compiler
             // Check for errors
             if (syntaxCheckErrorListener.errorReports.Count > 0) {
                 throw new VineParseException(syntaxCheckErrorListener.errorReports);
+            }
+        }
+
+        private List<SyntaxErrorReport> OptimizedParse(string vinecode)
+        {
+            // try with simpler/faster SLL(*)
+            parser.Interpreter.PredictionMode = PredictionMode.Sll;
+
+            // we don't want error messages or recovery during first try
+            parser.RemoveErrorListeners();
+            parser.AddErrorListener(new BailErrorListener());
+            parser.ErrorHandler = new BailErrorStrategy();
+            try
+            {
+                tree = parser.passage();
+                parsed = true;
+                // if we get here, there was no syntax error and SLL(*) was enough;
+                // there is no need to try full LL(*)
+                return null;
+            }
+            catch (ParseCanceledException e) // thrown by BailErrorStrategy
+            {
+                // rewind input stream
+                tokens.Reset();
+                parser.Reset();
+
+                // back to standard handler
+                //parser.AddErrorListener(ConsoleErrorListener<IToken>.Instance);
+                parser.ErrorHandler = new DefaultErrorStrategy();
+
+                // back to error reporting listener
+                parser.RemoveErrorListeners();
+                SyntaxCheckErrorListener syntaxCheckErrorListener = new SyntaxCheckErrorListener();
+                parser.AddErrorListener(syntaxCheckErrorListener);
+
+                // full now with full LL(*)
+                parser.Interpreter.PredictionMode = PredictionMode.Ll;
+                tree = parser.passage();
+                parsed = true;
+                
+                // returns errors
+                return syntaxCheckErrorListener.errorReports;
             }
         }
 
