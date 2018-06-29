26a58bee03c8873f673a88482ad9d4800270736f
Julien Samama <allinlabs@gmail.com>
2018-06-29 08:47:12 +0200

    VineBinding: removed 2nd constructor that was there to specify a type for each argument
    It wasn't a real helpful feature and it wasn't implemented anyway

--- VineScript/Core/Binding/VineBinding.cs
+++ VineScript/Core/Binding/VineBinding.cs
@@ -10,24 +10,11 @@ namespace VineScript.Binding
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     public class VineBinding : Attribute
     {
-        public Type[] Values { get; private set; }
-
         /// <summary>
         /// Method attribute that allows to bind a method to VineScript.
         /// </summary>
         public VineBinding()
         {
-        }
-
-        /// <summary>
-        /// Method attribute that allows to bind a method to VineScript.
-        /// </summary>
-        /// <param name="types">
-        /// [Not Implemented] Specify the expected type for each argument (optional)
-        /// </param>
-        public VineBinding(params Type[] types)
-        {
-            Values = types;
         }
     }
 }
